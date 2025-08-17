4d32296198b41f16621ffd5303626d30df15a899
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-08-17 19:13:12 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Experiements with the python library ruamel.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # Define 'forAllSystems' for properties that shall be build for x86_64 *and* aarch64
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (system: {
+      default = pkgs.${system}.mkShell {
+        buildInputs = with pkgs.${system}; [
+          # basics
+          gitMinimal
+
+          #python
+          python313
+          ruff
+          python313Packages.ruamel-yaml
+
+          # nix
+          nil
+          alejandra
+        ];
+      };
+    });
+  };
+}
